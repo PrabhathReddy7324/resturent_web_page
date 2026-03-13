6f5126ecdbf1ab6fed2477561c7031291efec450
Prabhath Reddy <prabhathbdl@gmail.com>
2026-03-14 02:50:19 +0530

    re

--- frontend/test_axios.cjs
+++
@@ -1,5 +0,0 @@
-const axios = require('axios');
-const api = axios.create({ baseURL: 'https://backend.com/api' });
-console.log(api.getUri({ url: '/admin/login' }));
-console.log(api.getUri({ url: 'admin/login' }));
-console.log(api.getUri({ baseURL: 'https://backend.com', url: '/admin/login' }));
